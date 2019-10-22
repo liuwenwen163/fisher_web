21119c280421b8b43c7c9a8d22aeff44d9234ca6
liuwenwen163 <liuwenwen163@gamil.com>
2019-10-22 22:05:40 +0800

    7-7 Translate python object to json string.

--- app/web/book.py
+++ app/web/book.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+import json
 from flask import jsonify,request
 
 from app.forms.book import SearchForm
@@ -19,17 +20,20 @@ def search():
     """
     form = SearchForm(request.args)
     books = BookCollection()
+
     if form.validate():
         q = form.q.data.strip()  # 去掉page前后的空格
         page = form.page.data
         key_or_isbn = is_key_or_isbn(q)
         yushu_book = YuShuBook()
+
         if key_or_isbn == "isbn":
             yushu_book.search_by_isbn(q)
         else:
             yushu_book.search_by_keyword(q, page)
+
         books.fill(yushu_book, q)
-        return jsonify(books)
+        return json.dumps(books, default=lambda o: o.__dict__)
     else:
         # flask的视图函数没有return语句是会报错的
         return jsonify(form.errors)
