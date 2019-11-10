f08d1f28ab17524d454364f06cc011066326f5f3
liuwenwen163 <liuwenwen163@gamil.com>
2019-11-10 20:57:27 +0800

    12-24 完成赠送礼物和心愿清单的撤销功能

--- app/web/gift.py
+++ app/web/gift.py
@@ -2,6 +2,8 @@ from flask import flash, current_app, redirect, url_for, render_template
 from flask_login import login_required,current_user
 
 from app import db
+from app.libs.enums import PendingStatus
+from app.models.drift import Drift
 from app.models.gift import Gift
 from app.view_models.gift import MyGifts
 from . import web
@@ -19,7 +21,6 @@ def my_gifts():
     return render_template('my_gifts.html', gifts=view_model.gifts)
 
 
-
 @web.route('/gifts/book/<isbn>')
 @login_required
 def save_to_gifts(isbn):
@@ -35,9 +36,27 @@ def save_to_gifts(isbn):
         flash('这本书已经存在于你的赠送清单或已存在于你的心愿清单，请不要重复添加')
     return redirect(url_for('web.book_detail', isbn=isbn))
 
+
 @web.route('/gifts/<gid>/redraw')
+@login_required
 def redraw_from_gifts(gid):
-    pass
+    gift = Gift.query.filter_by(
+        uid=current_user.id,
+        id=gid,
+        launched=False
+    ).first_or_404()
+    drift = Drift.query.filter_by(
+        gifter_id=current_user.id,
+        gift_id=gid,
+        _pending=PendingStatus.Waiting.value
+    ).first()
+    if drift:
+        flash("这个礼物正处于交易状态，请先前往鱼漂完成该交易")
+    else:
+        with db.auto_commit():
+            current_user.beans -= current_app.config['BEANS_UPLOAD_ONE_BOOK']
+            gift.delete()
+    return redirect(url_for('web.my_gifts'))
 
 
 
